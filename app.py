a83bb38708c20d75baa773c43b6fbc47ed1c701f
Ivan Demin <IvanDemin3467@gmail.com>
2021-09-07 09:00:42 +0600

    implemented: read options from file

--- app.py
+++ app.py
@@ -2,10 +2,58 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+OPTIONS_FILE = "options.txt"
+
+
+class Repo:
+    def __init__(self):
+        self.options = self.get_options()
+
+    def get_options(self):
+        """
+        It reads parameters for query from file: lower_date, upper_date, first_week
+        Those params are to be passed to function list_events_by_guest()
+        which lists events, filtered by params
+        Input = file name
+        Output = dict of options
+        """
+
+        options = {"use_db": False, "username": None, "password": None}
+
+        try:
+            s = open(OPTIONS_FILE, "rt", encoding="utf-8")
+            stream = list(s)
+            s.close()
+        except:
+            flash("Error while dealing with options file")
+            return options
+
+        for line in stream:
+            # do not read comments
+            if line.lstrip().startswith("#"):
+                continue
+            line = line.rstrip("\n")
+            # read content of string
+            fragments = line.split(":")
+            # do we use db?
+            if "use_db" in fragments[0]:
+                if "True" in fragments[1]:
+                    options["use_db"] = True
+            # username to connect to db
+            elif "username" in fragments[0]:
+                options["username"] = fragments[1]
+            # password to connect to db
+            elif "password" in fragments[0]:
+                options["password"] = fragments[1]
+
+        return options
+
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def get_user(user_id):
     conn = get_db_connection()
@@ -16,8 +64,11 @@ def get_user(user_id):
         abort(404)
     return user
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gh5ng843bh68hfi4nfc6h3ndh4xc53b56lk89gm4bf2gc6ehm'
+repo = Repo()
+
 
 @app.route('/')
 def index():
@@ -31,7 +82,6 @@ def index():
 def user(user_id):
     user = get_user(user_id)
     return render_template('user.html', user=user)
-
 
 
 @app.route('/create', methods=('GET', 'POST'))
